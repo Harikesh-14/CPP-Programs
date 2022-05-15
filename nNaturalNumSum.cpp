84892842ad6b001923d5c63f9145ab873a00679e
Harikesh Ranjan Sinha <68147228+Harikesh-14@users.noreply.github.com>
2022-05-15 14:57:29 +0530

    Add files via upload
    This program finds the sum of n natural numbers.

---
+++ nNaturalNumSum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int sum(int num1){
+    int add = 0;
+    for(int i=0; i<=num1;i++){
+        add += i;
+    }
+    return add;
+}
+int main(){
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    cout<<sum(num);
+}
